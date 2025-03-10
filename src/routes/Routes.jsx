bdc863947e7ef5a10b0b194979e116aa7fd903bf
rohi-code <rohithkonda99@gmail.com>
2025-03-10 09:40:22 +0530

    first commit

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,10 @@
+// routes/Routes.jsx
+import LoginPage from '../pages/Login'; // Correct path to Login.jsx
+import SignupPage from '../pages/Signup';
+import ActivationPage from '../pages/Activation'; // Correct path to Activation.jsx
+import HomePage from '../pages/HomePage';
+import CreateProduct from '../pages/CreateProduct'
+import MyProduct from '../pages/myProduct'
+import ProductDetail from '../pages/ProductDetail';
+import Cart from '../pages/cart';
+export { LoginPage,SignupPage,ActivationPage, HomePage,CreateProduct, MyProduct ,ProductDetail , Cart }; // Named export
